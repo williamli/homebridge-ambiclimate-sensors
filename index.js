3f96374f030b4af78decdc5f34124bae3a2a4134
William Li <williamli@bbi.io>
2020-05-07 03:34:32 +0800

    disable fanService and switchService

--- index.js
+++ index.js
@@ -64,8 +64,6 @@ function AmbiClimate(log, config) {
 
   this.temperatureService = new Service.TemperatureSensor(this.name);
   this.humidityService = new Service.HumiditySensor(this.name);
-  this.switchService = new Service.Switch(this.name);
-  this.fanService = new Service.Fan(this.name);
   this.informationService = new Service.AccessoryInformation();
 }
 
@@ -87,161 +85,7 @@ AmbiClimate.prototype = {
       err ? callback(err, data) : callback(err, data[0].value);
     });
   },
-  // Checks whether the current retrieved value is the same as the
-  // Homekit On mode for the device
-  getMode: function(callback) {
-    var accessory = this;
-
-    accessory.client.mode(accessory.settings, function(err, data) {
-      if (err) {
-        callback(err);
-      } else {
-        accessory.state.on = accessory.on.mode == data.mode;
-        callback(err, accessory.state.on);
-      }
-    });
-  },
-  // Sets the Ambi Climate Mode based on a switch.
-  setMode: function(callback) {
-    var accessory = this;
-    var settings = accessory.settings;
-
-    function setAmbiMode(state) {
-      switch (state.mode) {
-        case "Comfort":
-          accessory.log("Putting into comfort mode");
-          accessory.client.comfort(settings, function(err, data) {
-            callback(err);
-          });
-          break;
-        case "Off":
-          accessory.log("Turning off");
-          accessory.client.off(settings, function(err, data) {
-            callback(err);
-          });
-          break;
-        case "Away_Temperature_Upper":
-          accessory.log("Putting into away temperature upper mode");
-          settings.value = state.value;
-          accessory.client.away_temperature_upper(settings, function(
-            err,
-            data
-          ) {
-            callback(err);
-          });
-          break;
-        case "Away_Temperature_Lower":
-          accessory.log("Putting into away temperature lower mode");
-          settings.value = state.value;
-          accessory.client.away_temperature_lower(settings, function(
-            err,
-            data
-          ) {
-            callback(err);
-          });
-          break;
-        case "Away_Humidity_Upper":
-          accessory.log("Putting into away humidity upper mode");
-          settings.value = state.value;
-          accessory.client.away_humidity_upper(settings, function(err, data) {
-            callback(err);
-          });
-          break;
-        case "Temperature":
-          accessory.log("Putting into temperature mode");
-          settings.value = state.value;
-          accessory.client.temperature(settings, function(err, data) {
-            callback(err);
-          });
-          break;
-        default:
-          if (accessory.state.on) {
-            accessory.log("Putting into comfort mode");
-            accessory.client.comfort(accessory.settings, function(err, data) {
-              callback(err);
-            });
-          } else {
-            accessory.log("Turning off");
-            accessory.client.off(accessory.settings, function(err, data) {
-              callback(err);
-            });
-          }
-          break;
-      }
-    }
-
-    if (accessory.state.on) {
-      setAmbiMode(accessory.on);
-    } else {
-      setAmbiMode(accessory.off);
-    }
-  },
-  getActive: function(callback) {
-    this.client
-      .mode(this.settings)
-      .then(data => {
-        this.log("getActive: Retrieved mode is " + data.mode);
-        switch (data.mode) {
-          case "Off":
-          case "Manual":
-            callback(null, Characteristic.Active.INACTIVE);
-            break;
-          default:
-            callback(null, Characteristic.Active.ACTIVE);
-            break;
-        }
-      })
-      .catch(reason => {
-        callback(reason);
-      });
-  },
-  // The states returned by appliance_states do not reflect the current state
-  // of the air conditioner if it is off.  As a consequence we first check the
-  // Ambi Climate mode, and only retrieve appliance states when the mode is
-  // neither Off or Manual.
-  getRotationSpeed: function(callback) {
-    this.client
-      .mode(this.settings)
-      .then(data => {
-        switch (data.mode) {
-          case "Off":
-          case "Manual":
-            callback(null, 0);
-            break;
-          default:
-            this.client.appliance_states(this.settings).then(data => {
-              var rotationSpeed;
-              switch (data.data[0].fan) {
-                case "High":
-                  rotationSpeed = 100;
-                  break;
-                case "Med-High":
-                  rotationSpeed = 75;
-                  break;
-                case "Auto":
-                case "Med":
-                  rotationSpeed = 50;
-                  break;
-                case "Quiet":
-                case "Med-Low":
-                  rotationSpeed = 38;
-                  break;
-                case "Low":
-                  rotationSpeed = 25;
-                  break;
-                default:
-                  rotationSpeed = 0;
-                  break;
-              }
-              callback(null, rotationSpeed);
-            });
-            break;
-        }
-      })
-      .catch(reason => {
-        callback(reason);
-      });
-  },
+  
 
   //
   // Services
@@ -275,51 +119,6 @@ AmbiClimate.prototype = {
         }.bind(this)
       );
 
-    this.switchService
-      .getCharacteristic(Characteristic.On)
-      .on(
-        "get",
-        function(callback) {
-          this.getMode(
-            function(error, data) {
-              callback(error, data);
-            }.bind(this)
-          );
-        }.bind(this)
-      )
-      .on(
-        "set",
-        function(value, callback) {
-          this.state.on = value;
-          this.setMode(
-            function(error, data) {
-              callback(error);
-            }.bind(this)
-          );
-        }.bind(this)
-      );
-
-    this.fanService.getCharacteristic(Characteristic.On).on(
-      "get",
-      function(callback) {
-        this.getActive(
-          function(error, data) {
-            callback(error, data);
-          }.bind(this)
-        );
-      }.bind(this)
-    );
-
-    this.fanService.getCharacteristic(Characteristic.RotationSpeed).on(
-      "get",
-      function(callback) {
-        this.getRotationSpeed(
-          function(error, data) {
-            callback(error, data);
-          }.bind(this)
-        );
-      }.bind(this)
-    );
 
     this.informationService
       .setCharacteristic(Characteristic.Manufacturer, "Ambi Labs")
@@ -329,8 +128,6 @@ AmbiClimate.prototype = {
     return [
       this.temperatureService,
       this.humidityService,
-      this.switchService,
-      this.fanService,
       this.informationService
     ];
   }
